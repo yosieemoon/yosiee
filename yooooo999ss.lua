50555d643158b133a3dd261f7a5e991b3448284b
yosieemoon <yosapat4@gmail.com>
2026-01-04 15:11:35 +0700

    Update yooooo999ss.lua

--- yooooo999ss.lua
+++ yooooo999ss.lua
@@ -72,8 +72,8 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Start Farm"] = true,
         ["Minimum Rod"] = "Ghostfinn Rod",
         ["Event List"] = {
-            ["Christmas Cave"] = true,
-            ["Hacker Event"] = true,
+            ["Christmas Cave"] = false,
+            ["Hacker Event"] = false,
             
         },
     },
@@ -152,6 +152,7 @@ script_key="7ECF8FC9340DD1F67092AC7E1E16CC04";
 
 local s,r repeat s,r=pcall(function()return game:HttpGet("https://api.wintercode.dev/loader/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/list/refs/heads/main/game"))()
+
 
 
 
