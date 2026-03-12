7ba5891e4eeb5ae79aa8d41cf818387f7a6f2473
yosieemoon <yosapat4@gmail.com>
2026-03-13 01:10:01 +0700

    Update yooooo999ss.lua

--- yooooo999ss.lua
+++ yooooo999ss.lua
@@ -35,7 +35,7 @@ _G.FishItConfig = _G.FishItConfig or {
     ["Doing Quest"] = {
         ["Auto Ghostfinn Rod"] = true,
         ["Auto Element Rod"] = false,
- 	["Unlock Ancient Ruin"] = false,
+ 	["Unlock Ancient Ruin"] = true,
         ["Allowed Sacrifice"] = {
             "Blob Shark",
             "Ghost Shark",
@@ -48,10 +48,8 @@ _G.FishItConfig = _G.FishItConfig or {
             "Giant Squid",
             "Depthseeker Ray",
             "Bone Whale",
-            "Queen Crab",
-            "King Crab",
         },
-        ["FARM_LOC_SECRET_SACRIFICE"] = "Treasure Room",
+        ["FARM_LOC_SECRET_SACRIFICE"] = "Ocean",
 
         ["Minimum Rod"] = "Astral Rod",
     },
@@ -59,12 +57,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Link Webhook"] = "https://discord.com/api/webhooks/1415211763091247155/oPwylc3-SPn35TcC7mZKmFPnbGSqGa6xOqRAD2FMkBOLb0PbGRpkfcT_jgF6kS0gNMpX",
         ["Auto Sending"] = true,
         ["Category"] = {"Secret"},
-        {Name = "Ruby", Variant = "Gemstone"},
-    },
-    ["Item Name"] = {
-        "Evolved Enchant Stone",
-    },
-    ["Link Webhook"] = "https://discordapp.com/api/webhooks/1481702024050643197/Aoayl7rUYtxZ3UrHS6hNLe9yKe0z_4LY39xjp9Kxoxn0rTCBMOoUM6RkV1gHQN8706E0",
+
         ["Link Webhook Quest Complete"] = "https://discord.com/api/webhooks/1449824144286945351/KFJOtI1N9AcNg3EXjXAGq7l90brfg2BIDuh1lOoSH-0YqwrI96tZIGuMPz4gjY3-OPPr",
     },
     ["Weather"] = {
@@ -84,7 +77,8 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Auto Use"] = true,
         ["Minimum Rod"] = "Ghostfinn Rod",
         ["Buy List"] = {
-            ["Mutation Totem"] = 10,
+            "Luck Totem",
+            "Mutation Totem",
         },
     },
     ["Event"] = {
@@ -101,7 +95,13 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Roll Enchant"] = false,
         ["Evolved Roll Enchant"] = false, -- true = Roll Evolved Stone until get ANY evolved-only enchant from list, then fallback to regular stone
         ["Enchant List"] = {
-            "Cursed I",
+            "Reeler I",
+            "Empowered I",
+            "Big Hunter I",
+            "Mutation Hunter II",
+            "Prismatic",
+            "SECRET Hunter",
+            "Reeler II",
         },
         ["Second Enchant"] = true,
         ["Allowed Sacrifice"] = {
@@ -116,11 +116,14 @@ _G.FishItConfig = _G.FishItConfig or {
             "Giant Squid",
             "Depthseeker Ray",
             "Bone Whale",
-            "Queen Crab",
-            "King Crab",
         },
         ["Second Enchant List"] = {
-            "Cursed I",
+            "Perfection",
+            "Reeler I",
+            "Empowered I",
+            "Big Hunter I",
+            "Mutation Hunter I",
+            
         },
         ["Minimum Rod"] = "Ghostfinn Rod",
     },
@@ -146,8 +149,8 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Location Rods"] = { -- Farm location and minimum rod to start on that location, also only 1 rod and only 1 location
             ["Fisherman Island"] = {"Starter Rod"},
             ["Kohana Volcano"] = {"Grass Rod"},
-            ["Tropical Grove"] = {"Astral Rod","Midnight Rod"},
-            ["Treasure Room"] = {"Element Rod","Ghostfinn Rod", "Angler Rod", "Ares Rod", "Bamboo Rod"},
+            ["Treasure Room"] = {"Astral Rod","Midnight Rod"},
+            ["Ancient Ruin"] = {"Element Rod","Ghostfinn Rod", "Angler Rod", "Ares Rod", "Bamboo Rod"},
         },
     },
 
@@ -169,8 +172,6 @@ loadstring(game:HttpGet("https://sc.pebletz.xyz/loader?t=disconnect_detector"))(
 
 local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/list/refs/heads/main/game"))()
-
-
 
 
 
