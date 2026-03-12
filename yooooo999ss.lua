a8d65c619414be8a31dcbe15d5f747d0bd73bde2
yosieemoon <yosapat4@gmail.com>
2026-03-13 01:46:04 +0700

    Update yooooo999ss.lua

--- yooooo999ss.lua
+++ yooooo999ss.lua
@@ -56,14 +56,13 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Minimum Rod"] = "Astral Rod",
     },
     ["WebHook"] = {
-        ["Link Webhook"] = "https://discord.com/api/webhooks/1415211763091247155/oPwylc3-SPn35TcC7mZKmFPnbGSqGa6xOqRAD2FMkBOLb0PbGRpkfcT_jgF6kS0gNMpX",
         ["Auto Sending"] = true,
-        ["Category"] = {"Secret"},
-                       {Name = "Ruby", Variant = "Gemstone"},
-    },
-        ["Item Name"] = {
-            "Evolved Enchant Stone",
-    },              
+        ["Category"] = {
+            "Secret",
+            {Name = "Ruby", Variant = "Gemstone"},
+        },
+        ["Item Name"] = {"Evolved Enchant Stone",},
+        ["Link Webhook"] = "https://discord.com/api/webhooks/1415211763091247155/oPwylc3-SPn35TcC7mZKmFPnbGSqGa6xOqRAD2FMkBOLb0PbGRpkfcT_jgF6kS0gNMpX",
         ["Link Webhook Quest Complete"] = "https://discord.com/api/webhooks/1449824144286945351/KFJOtI1N9AcNg3EXjXAGq7l90brfg2BIDuh1lOoSH-0YqwrI96tZIGuMPz4gjY3-OPPr",
     },
     ["Weather"] = {
@@ -170,6 +169,7 @@ loadstring(game:HttpGet("https://sc.pebletz.xyz/loader?t=disconnect_detector"))(
 
 local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/list/refs/heads/main/game"))()
+
 
 
 
