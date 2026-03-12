62c613e0ff8baaf583f00fd073fe30d27ed86e77
yosieemoon <yosapat4@gmail.com>
2026-03-13 01:23:40 +0700

    Update yooooo999ss.lua

--- yooooo999ss.lua
+++ yooooo999ss.lua
@@ -77,8 +77,7 @@ _G.FishItConfig = _G.FishItConfig or {
         ["Auto Use"] = true,
         ["Minimum Rod"] = "Ghostfinn Rod",
         ["Buy List"] = {
-            "Luck Totem",
-            "Mutation Totem",
+            ["Mutation Totem"] = 5,
         },
     },
     ["Event"] = {
@@ -172,6 +171,7 @@ loadstring(game:HttpGet("https://sc.pebletz.xyz/loader?t=disconnect_detector"))(
 
 local s,r repeat s,r=pcall(function()return game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/roblog/refs/heads/main/fishit-78c86024ea87c8eca577549807421962.lua")end)wait(1)until s;loadstring(r)()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/FnDXueyi/list/refs/heads/main/game"))()
+
 
 
 
