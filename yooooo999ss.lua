c73028d7db990b6e2911cf0f8a99782f708096e5
yosieemoon <yosapat4@gmail.com>
2026-05-10 18:06:40 +0700

    Update yooooo999ss.lua

--- yooooo999ss.lua
+++ yooooo999ss.lua
@@ -29,7 +29,7 @@ _G.FishItConfig = _G.FishItConfig or {
     },
     ["Selling"] = {
         ["Auto Sell"] = true,
-        ["Auto Sell Threshold"] = "Legendary",
+        ["Auto Sell Threshold"] = "mythic",
         ["Auto Sell Every"] = 100,
     },
     ["Doing Quest"] = {
