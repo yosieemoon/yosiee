1e978dfb7504e6e9b8c8c6ac65acaaef7e8f72e1
yosieemoon <yosapat4@gmail.com>
2026-05-20 20:15:50 +0700

    Update yooooo999ss.lua

--- yooooo999ss.lua
+++ yooooo999ss.lua
@@ -150,7 +150,7 @@ _G.FishItConfig = _G.FishItConfig or {
             ["Fisherman Island"] = {"Starter Rod", "Luck Rod"},
             ["Kohana Volcano"] = {"Grass Rod", "Midnight Rod"},
             ["Tropical Grove"] = {"Astral Rod"},
-            ["Treasure Room"] = {"Diamond Rod", "Element Rod", "Ghostfinn Rod", "Angler Rod", "Ares Rod"},
+            ["Underwater City"] = {"Diamond Rod", "Element Rod", "Ghostfinn Rod", "Angler Rod", "Ares Rod"},
         },
     },
 
